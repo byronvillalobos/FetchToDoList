78ebbb164544d4c1efd9d879798d0b52448de73f
byronvillalobos <byronvillalobos@hotmail.com>
2021-03-09 21:59:07 +0000

    comments

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,62 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [inputValue, setInputValue] = React.useState("");
+	const [list, setlist] = React.useState([]);
+	let countItems = list.length;
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 text-center">
+			<div>
+				<div>
+					{" "}
+					<h1>To Do List </h1>{" "}
+				</div>
+
+				<input
+					id="myInput"
+					type="text"
+					label="Task"
+					placeholder="input a value"
+					onChange={e => {
+						setInputValue(e.target.value);
+					}}
+					value={inputValue}
+					onKeyPress={e => {
+						if (e.key === "Enter") {
+							setlist(list.concat(inputValue));
+							setInputValue("");
+						}
+					}}
+				/>
+				<div className="border border-dark">
+					<dl>
+						{list.map(name => (
+							<dt key={name}> {name} </dt>
+						))}
+					</dl>
+				</div>
+
+				<div>
+					<input
+						className="btn btn-primary"
+						type="reset"
+						onClick={e => {
+							setlist([]);
+						}}
+						value="Reset"></input>
+					<p>Total of tasks: {countItems}</p>
+				</div>
+			</div>
+			{/* <input
+				className="btn btn-primary"
+				type="Submit"
+				onClick={e => {
+					setlist("");
+				}}
+				value="Submit"></input> */}
+
+			{/* for individual deletion https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array */}
 		</div>
 	);
 }
